5cbca18959871e831af4ccac54dd2e42e623759a
Hefler <heflerdev@gmail.com>
2021-02-27 01:26:33 -0300

    calculator style & responsiveness
    Finished both calculator style and responsiveness by adding the CSStack framework and implementing conditional styling to elements.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import 'csstack';
 
 ReactDOM.render(<App className="main-app" />, document.getElementById('root'));
