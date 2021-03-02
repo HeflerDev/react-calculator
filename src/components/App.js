d76b5699d2a7cc60479301a38584b11eff7f775d
Hefler <heflerdev@gmail.com>
2021-03-02 19:13:38 -0300

    final styling, final structure, responsiveness

--- src/components/App.js
+++ src/components/App.js
@@ -29,8 +29,23 @@ const App = () => {
 
   return (
     <>
-      <Display result={getters} />
-      <ButtonPanel clickHandler={handleClick} />
+      <div className="board around">
+        <div className="col-12 col-m-4 col-l-7">
+          <div className="content-container">
+            <div className="m-auto">
+              <h3>Let&apos;s make some Math!</h3>
+              <p>
+                The App work as a normal and functional calculator, just insert the numbers
+                and calculate the results.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-m-8 col-l-3">
+          <Display result={getters} />
+          <ButtonPanel clickHandler={handleClick} />
+        </div>
+      </div>
     </>
   );
 };
